25b140c55f129432b88727533b8f90d920d04a6e
(R.R.) Moelker <r@m.com>
2016-01-19 19:43:22 +0100

    pip package configuration files.

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-markdownify',
+    version='0.1.0',
+    packages=['markdownify'],
+    include_package_data=True,
+    license='MIT',
+    description='Markdown template filter for Django.',
+    long_description=README,
+    url='https://github.com/RRMoelker/django-markdownify',
+    author='R Moelker',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'Django',
+        'markdown',
+        'bleach',
+    ],
+)
