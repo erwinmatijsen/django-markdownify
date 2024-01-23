13257db8c27fd4bc2f8485b4edd344ade4c03724
Erwin Matijsen <erwin@erwinmatijsen.nl>
2024-01-23 10:44:22 +0100

    Update docs settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,7 +65,7 @@ release = '1.0.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -166,7 +166,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     (master_doc, 'Markdownify', 'Markdownify Documentation',
-     author, 'Markdownify', 'One line description of project.',
+     author, 'Markdownify', 'A Django Markdown filter.',
      'Miscellaneous'),
 ]
 
