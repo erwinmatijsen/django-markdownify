5c3e3705f2b31c5f0f017f007db519960f415566
Erwin Matijsen <erwin@erwinmatijsen.nl>
2026-05-18 08:50:45 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-markdownify',
-    version='0.9.6',
+    version='0.9.7',
     packages=['markdownify'],
     package_dir={'markdownify': 'markdownify'},
     package_data={'markdownify': ['tests/*.md']},
@@ -18,7 +18,7 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     url='https://github.com/erwinmatijsen/django-markdownify',
-    download_url='https://github.com/erwinmatijsen/django-markdownify/archive/0.9.6.tar.gz',
+    download_url='https://github.com/erwinmatijsen/django-markdownify/archive/0.9.7.tar.gz',
     author='Erwin Matijsen, R Moelker',
     author_email='erwin@erwinmatijsen.nl',
     python_requires=">=3.7",
